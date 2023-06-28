ae396dafaae4d9586c91d0dd4fe5f30de7322dad
Anastasiia <Batrakovaad@yndex.ru>
2023-06-28 22:33:52 +0700

    finish3

---
+++ Homework/Task1.py
@@ -0,0 +1,14 @@
+# За день машина проезжает n километров. Сколько
+# дней нужно, чтобы проехать маршрут длиной m
+# километров? При решении этой задачи нельзя
+# пользоваться условной инструкцией if и циклами.
+# Input:
+# n = 700
+# m = 750
+# Output:
+# 2
+
+speed = int(input("Input Spacing per day - "))
+spacing = int(input("Input total Spacing - "))
+
+print((spacing+speed-1)//speed)
