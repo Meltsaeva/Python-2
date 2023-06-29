b920d5b00f84daa2af0de6d26ea36016fde55a71
Anastasiia <Batrakovaad@yndex.ru>
2023-06-29 12:52:34 +0700

    finish27

--- Homework/Task27.py
+++ Homework/Task27.py
@@ -10,8 +10,19 @@
 # Output: 13
 
 
-sentence = str(("She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells").upper())
-print(sentence)
-new_sentence = set(sentence.split())
-print(new_sentence)
-print(len(new_sentence))
+# sentence = str(("She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells").upper())
+# print(sentence)
+# new_sentence = set(sentence.split())
+# print(new_sentence)
+# print(len(new_sentence))
+
+from string import punctuation
+
+text = " вот текст с презентации: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+
+for ch in punctuation + r'\n':
+    text = text.replace(ch, '')
+
+word = text.lower().split()
+different_words_counter = len(set(word))
+print(different_words_counter)
