e57ee8767572368a1949c109207bb8b68732b6d6
Anastasiia <Batrakovaad@yndex.ru>
2023-07-03 12:58:28 +0700

    finish28

--- Homework/Task28.py
+++ Homework/Task28.py
@@ -4,3 +4,14 @@
 # Также нельзя использовать циклы.
 # 2 2
 # 4
+
+first = int(input("Input the first number - "))
+second = int(input("Input the second number -  "))
+
+def sum_1(first, second):
+    if first == 0:
+        return second
+    else:
+        return sum_1(first - 1, second + 1)
+    
+print(sum_1(first, second))
