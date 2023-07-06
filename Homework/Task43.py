20dd8d4462ed7dd7cccbf621f9403a080c5dd1c5
Anastasiia <Batrakovaad@yndex.ru>
2023-07-06 12:45:10 +0700

    finish43

--- Homework/Task43.py
+++ Homework/Task43.py
@@ -7,3 +7,9 @@
 # Ввод: Вывод:
 # 1 2 3 2 3 2
 
+from random import randint
+print(my_list:= [randint(1, 10) for _ in range(int(input("Input a number of numbers - ")))])
+count, result = 0, 0
+for item in set(my_list):
+    result += my_list.count(item)//2
+print(result)
