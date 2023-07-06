fa8600526f0fa6c8b6f4613beb957c3314615713
Anastasiia <Batrakovaad@yndex.ru>
2023-07-06 12:34:42 +0700

    finish43

--- Homework/Task39.py
+++ Homework/Task39.py
@@ -14,14 +14,11 @@ first_number = int(input("Input a number of the first list - "))
 second_number = int(input("Input a number of the second list - "))
 
 from random import randint
-first_list = [randint(1, 10) for _ in range(first_number)]
-second_list = [randint(1, 10) for _ in range(second_number)]
-print(first_list, second_list, sep='\n')
-third_list = []
-
-for i in range(len(first_list)):
-    if second_list.count(first_list[i]) == 0:
-        third_list.append(first_list[i])
-
-print(third_list)
-
+print(first_list := [randint(1, 10) for _ in range(first_number)])
+print(second_list := [randint(1, 10) for _ in range(second_number)])
+# third_list = []
+# for i in range(len(first_list)):
+#     if second_list.count(first_list[i]) == 0:
+#         third_list.append(first_list[i])
+# print(third_list)
+print([item for item in first_list if item not in second_list])
